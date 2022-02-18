da479bc56a348e43021edd5f04d8176547dff7b9
Sindre <siditlef@gmail.com>
2022-02-18 15:27:36 +0100

    m

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="Guess the Number" />
+
+
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1 /* Take full width and height */
   },
 });
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const StartGameScreen = props => {
+    return (
+        <View style={styles.screen}>
+            <Text>xx</Text>
+        </View>
+
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1, /*takes all the space left */
+        padding: 10,
+        alignItems: 'center',
+    }
+
+});
+
+export default StartGameScreen;
